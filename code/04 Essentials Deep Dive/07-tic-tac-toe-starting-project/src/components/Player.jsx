6247012b8d7ff57a5deb6cfeef28d24e68280630
Anastasia Mexa <anastasia.mexa@yahoo.gr>
2023-11-09 22:03:27 +0200

    Created player component

---
+++ code/04 Essentials Deep Dive/07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -0,0 +1,31 @@
+import {useState} from 'react';
+
+function Player({initialName, symbol}) {
+  const [playerName, setPlayerName] = useState(initialName);
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleEditing() {
+    setIsEditing(() => !isEditing);
+  }
+
+  function handleChanges(event) {
+    setPlayerName(event.target.value);
+  }
+
+  let editalbePlayerName = <span className="player-name">{playerName}</span>;
+  if (isEditing) {
+    editalbePlayerName = <input type="text" required value={playerName} onChange={handleChanges}></input>;
+  }
+
+  return (
+    <li>
+      <span className="player">
+        {editalbePlayerName}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditing}>{isEditing ? "Save" : "Edit"}</button>
+    </li>
+  );
+}
+
+export default Player;
