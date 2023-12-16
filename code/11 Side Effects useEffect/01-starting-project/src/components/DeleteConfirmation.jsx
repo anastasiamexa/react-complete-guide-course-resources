55eb3749653113760a48747010b56f62bc9afee3
Anastasia Mexa <anastasia.mexa@yahoo.gr>
2023-12-16 16:41:24 +0200

    Fix useEffect indentation

--- code/11 Side Effects useEffect/01-starting-project/src/components/DeleteConfirmation.jsx
+++ code/11 Side Effects useEffect/01-starting-project/src/components/DeleteConfirmation.jsx
@@ -15,7 +15,7 @@ export default function DeleteConfirmation({ onConfirm, onCancel }) {
     };
   }, []);
   
-  useEffect(() => {
+  useEffect(() => { 
     const timer = setTimeout(() => {
       onConfirm();
     }, TIMER);
