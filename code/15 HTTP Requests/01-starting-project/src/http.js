4dab5fdd4e455cca8fa296c2cbd0e0e25389c996
Anastasia Mexa <anastasia.mexa@yahoo.gr>
2023-12-24 20:27:14 +0200

    Add user places and handle error when updating places

--- code/15 HTTP Requests/01-starting-project/src/http.js
+++ code/15 HTTP Requests/01-starting-project/src/http.js
@@ -1,10 +1,29 @@
+// GET /places
 export async function fetchAvailablePlaces() {
     const response = await fetch('http://localhost:3000/places');
-    const resData = await response.json();
+    const data = await response.json();
 
     if (!response.ok) {
         throw new Error('Failed to fetch places');
     }
 
-    return resData.places;
+    return data.places;
+}
+
+// PUT /user-places
+export async function updateUserPlaces(places) {
+    const response = await fetch('http://localhost:3000/user-places', {
+        method: 'PUT',
+        body: JSON.stringify({ places }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+    const data = await response.json();
+
+    if (!response.ok) {
+        throw new Error('Failed to update user places');
+    }
+
+    return data.message;
 }
