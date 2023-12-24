f2b087cc1a733136430e9e543a03fdf097609b60
Anastasia Mexa <anastasia.mexa@yahoo.gr>
2023-12-24 19:50:48 +0200

    Add error handling and loading state to AvailablePlaces component

--- code/15 HTTP Requests/01-starting-project/src/components/AvailablePlaces.jsx
+++ code/15 HTTP Requests/01-starting-project/src/components/AvailablePlaces.jsx
@@ -1,18 +1,36 @@
 import { useState, useEffect } from 'react';
 import Places from './Places.jsx';
+import Error from './Error.jsx';
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [isFetching, setIsFetching] = useState(false);
   const [availablePlaces, setAvailablePlaces] = useState([]);
+  const [error, setError] = useState();
 
   // Make GET request to fetch available places
   // useEffect() is needed to avoid infinite loop
   useEffect(() => {
     fetch('http://localhost:3000/places').then((response) => {
-      response.json().then((data) => {
-        setAvailablePlaces(data.places);
-      });
+      setIsFetching(true);
+      try {
+        if (!response.ok) {
+          throw new Error('Failed to fetch places.');
+        }
+        response.json().then((data) => {
+          setAvailablePlaces(data.places);
+        });
+      } catch (error) {
+        setError({
+          message: error.message || 'Could not fetch places, please try again later.',
+        });
+      }
+      setIsFetching(false);
     })
   }, []);
+
+  if (error) {
+    return <Error title="An error occurred!" message={error.message} />;
+  }
 
   // Make GET request to fetch available places
   // useEffect() is needed to avoid infinite loop
@@ -31,7 +49,7 @@ export default function AvailablePlaces({ onSelectPlace }) {
     <Places
       title="Available Places"
       places={availablePlaces}
-      isLoading={availablePlaces.length === 0}
+      isLoading={isFetching}
       loadingText="Fetching places data..."
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
