d86aded444157e507629ed8b5f44e13053130db1
Anastasia Mexa <anastasia.mexa@yahoo.gr>
2023-12-24 18:47:20 +0200

    Add HTTP request to fetch available places

--- code/15 HTTP Requests/01-starting-project/src/components/AvailablePlaces.jsx
+++ code/15 HTTP Requests/01-starting-project/src/components/AvailablePlaces.jsx
@@ -1,10 +1,23 @@
+import { useState, useEffect } from 'react';
 import Places from './Places.jsx';
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [availablePlaces, setAvailablePlaces] = useState([]);
+
+  // Make GET request to fetch available places
+  // useEffect() is needed to avoid infinite loop
+  useEffect(() => {
+    fetch('http://localhost:3000/places').then((response) => {
+      response.json().then((data) => {
+        setAvailablePlaces(data.places);
+      });
+    })
+  }, []);
+
   return (
     <Places
       title="Available Places"
-      places={[]}
+      places={availablePlaces}
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
