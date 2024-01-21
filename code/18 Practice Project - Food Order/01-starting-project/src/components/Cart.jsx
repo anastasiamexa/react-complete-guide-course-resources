c55cd3292ce432af6b5b2f5df739e502606edfdb
Anastasia Mexa <anastasia.mexa@yahoo.gr>
2024-01-21 18:45:36 +0200

    Refactor Cart component to use CartItem component

--- code/18 Practice Project - Food Order/01-starting-project/src/components/Cart.jsx
+++ code/18 Practice Project - Food Order/01-starting-project/src/components/Cart.jsx
@@ -4,32 +4,42 @@ import UserProgressContext from "../store/UserProgressContext";
 import Modal from "./UI/Modal";
 import { currencyFormatter } from "../util/formatting";
 import Button from "./UI/Button";
+import CartItem from "./CartItem";
 
 function Cart() {
-    const cartCtx = useContext(CartContext);
-    const userProgressCtx = useContext(UserProgressContext);
+  const cartCtx = useContext(CartContext);
+  const userProgressCtx = useContext(UserProgressContext);
 
-    const cartTotal = cartCtx.items.reduce((total, item) => {
-        return total + item.price * item.quantity;
-    }, 0);
+  const cartTotal = cartCtx.items.reduce((total, item) => {
+    return total + item.price * item.quantity;
+  }, 0);
 
-    function handleCloseCart() {
-        userProgressCtx.hideCart();
-    }
+  function handleCloseCart() {
+    userProgressCtx.hideCart();
+  }
 
-    return <Modal className="cart" open={userProgressCtx.progress === 'cart'}>
-        <h2>Your Cart</h2>
-        <ul>
-            {cartCtx.items.map((item) => (
-                <li key={item.id}>{item.name} - {item.quantity}</li>
-            ))}
-        </ul>
-        <p className="cart-total">{currencyFormatter.format(cartTotal)}</p>
-        <p className="modal-actions">
-            <Button textOnly onClick={handleCloseCart}>Close</Button>
-            <Button onClick={handleCloseCart}>Go to Checkout</Button>
-        </p>
+  return (
+    <Modal className="cart" open={userProgressCtx.progress === "cart"}>
+      <h2>Your Cart</h2>
+      <ul>
+        {cartCtx.items.map((item) => (
+          <CartItem
+            key={item.id}
+            item={item}
+            onIncrease={() => cartCtx.addItem(item)}
+            onDecrease={() => cartCtx.removeItem(item.id)}
+          />
+        ))}
+      </ul>
+      <p className="cart-total">{currencyFormatter.format(cartTotal)}</p>
+      <p className="modal-actions">
+        <Button textOnly onClick={handleCloseCart}>
+          Close
+        </Button>
+        <Button onClick={handleCloseCart}>Go to Checkout</Button>
+      </p>
     </Modal>
+  );
 }
 
 export default Cart;
