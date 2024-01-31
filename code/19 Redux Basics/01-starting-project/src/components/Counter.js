81bc38266e57e291e812569f8b156d6ee3899b32
Anastasia Mexa <anastasia.mexa@yahoo.gr>
2024-01-31 23:41:24 +0200

    use redux toolkit

--- code/19 Redux Basics/01-starting-project/src/components/Counter.js
+++ code/19 Redux Basics/01-starting-project/src/components/Counter.js
@@ -1,4 +1,5 @@
 import { useDispatch, useSelector } from 'react-redux';
+import { counterActions } from '../store/index';
 import classes from './Counter.module.css';
 
 const Counter = () => {
@@ -10,19 +11,23 @@ const Counter = () => {
 
   // dispatch is a function that takes an action object as an argument.
   const incrementHandler = () => {
-    dispatch({ type: 'increment' });
+    //dispatch({ type: 'increment' });
+    dispatch(counterActions.increment());
   };
 
   const increaseHandler = () => {
-    dispatch({ type: 'increase', amount: 5});
+    //dispatch({ type: 'increase', amount: 5});
+    dispatch(counterActions.increase(5)); // {type: SOME_UNIQUE_IDENTIFIER, payload: 5}
   };
 
   const decrementHandler = () => {
-    dispatch({ type: 'decrement' });
+    //dispatch({ type: 'decrement' });
+    dispatch(counterActions.decrement());
   };
 
   const toggleCounterHandler = () => {
-    dispatch({ type: 'toggle' });
+    //dispatch({ type: 'toggle' });
+    dispatch(counterActions.toggle());
   };
 
   return (
--- code/19 Redux Basics/01-starting-project/src/store/index.js
+++ code/19 Redux Basics/01-starting-project/src/store/index.js
@@ -1,6 +1,30 @@
-import { createStore } from 'redux';
+//import { createStore } from 'redux';
+import { createSlice, configureStore } from '@reduxjs/toolkit';
 
-const counterReducer = (state = { counter: 0, showCounter: true }, action) => {
+const initialState = { counter: 0, showCounter: true }; // initial state
+
+// createSlice is a function that takes a configuration object as an argument.
+const counterSlice = createSlice({
+  name: 'counter',
+  initialState: initialState,
+  reducers: {
+    increment(state) {
+      state.counter++; // here this is allowed because of immer library
+    },
+    decrement(state) {
+      state.counter--;
+    },
+    increase(state, action) {
+      state.counter = state.counter + action.payload;
+    },
+    toggle(state) {
+      state.showCounter = !state.showCounter;
+    }
+  }
+});
+
+// counterReducer is a function that takes the current state and an action as arguments.
+/*const counterReducer = (state = initialState, action) => {
   if (action.type === 'increment') {
     return {
       counter: state.counter + 1,
@@ -31,7 +55,13 @@ const counterReducer = (state = { counter: 0, showCounter: true }, action) => {
 
   return state;
 }; // reducer function
+*/
 
-const store = createStore(counterReducer); // store is created
+//const store = createStore(counterReducer); // store is created
+const store = configureStore({
+  reducer: counterSlice.reducer
+}); // store is created
+
+export const counterActions = counterSlice.actions;
 
 export default store;
