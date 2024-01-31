a0cfaf9c6c719c15946492918ca6cffc467cb61e
Anastasia Mexa <anastasia.mexa@yahoo.gr>
2024-01-31 23:12:10 +0200

    Add Redux functionality to Counter component

--- code/19 Redux Basics/01-starting-project/src/components/Counter.js
+++ code/19 Redux Basics/01-starting-project/src/components/Counter.js
@@ -1,12 +1,31 @@
+import { useDispatch, useSelector } from 'react-redux';
 import classes from './Counter.module.css';
 
 const Counter = () => {
+  // useDispatch is a hook that returns a reference to the dispatch function from the Redux store.
+  const dispatch = useDispatch();
+  // useSelector is a hook that allows us to extract data from the Redux store state.
+  const counter = useSelector(state => state.counter);
+
+  // dispatch is a function that takes an action object as an argument.
+  const incrementHandler = () => {
+    dispatch({ type: 'increment' });
+  };
+
+  const decrementHandler = () => {
+    dispatch({ type: 'decrement' });
+  };
+
   const toggleCounterHandler = () => {};
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={decrementHandler}>Decrement</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
