018cb5a24fcb55503303e609367e524252f0896d
Anastasia Mexa <anastasia.mexa@yahoo.gr>
2024-01-31 22:59:05 +0200

    Create Redux store for React

---
+++ code/19 Redux Basics/01-starting-project/src/store/index.js
@@ -0,0 +1,21 @@
+import { createStore } from 'redux';
+
+const counterReducer = (state = { counter: 0 }, action) => {
+  if (action.type === 'increment') {
+    return {
+      counter: state.counter + 1,
+    };
+  }
+
+  if (action.type === 'decrement') {
+    return {
+      counter: state.counter - 1,
+    };
+  }
+
+  return state;
+}; // reducer function
+
+const store = createStore(counterReducer); // store is created
+
+export default store;
