b4a1e852f8bd7d245db6ac97c37d78d499c7cf66
Anastasia Mexa <anastasia.mexa@yahoo.gr>
2024-02-04 17:57:04 +0200

    Add layouts styling

--- code/21 Routing/01-starting-project/src/pages/Root.js
+++ code/21 Routing/01-starting-project/src/pages/Root.js
@@ -1,11 +1,14 @@
 import { Outlet } from "react-router-dom";
 import MainNavigation from "../components/MainNavigation";
+import classes from "./Root.module.css";
 
 export default function Root() {
   return (
     <>
       <MainNavigation />
-      <Outlet />
+      <main className={classes.content}>
+        <Outlet />
+      </main>
     </>
   );
 }
