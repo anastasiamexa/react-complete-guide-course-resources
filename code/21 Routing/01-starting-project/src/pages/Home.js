169b2f629d97bc88de26bc5827eb2f47a214ac94
Anastasia Mexa <anastasia.mexa@yahoo.gr>
2024-02-04 17:52:37 +0200

    wrapped routes

--- code/21 Routing/01-starting-project/src/pages/Home.js
+++ code/21 Routing/01-starting-project/src/pages/Home.js
@@ -4,7 +4,9 @@ export default function Home() {
   return (
     <>
       <h1>My Home Page</h1>
-      <p>Go to <Link to="/products">the list of products</Link></p>
+      <p>
+        Go to <Link to="/products">the list of products</Link>
+      </p>
     </>
   );
 }
