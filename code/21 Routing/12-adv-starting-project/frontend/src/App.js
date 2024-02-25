927ed1460369544f503b78a94476b32daa297d90
Anastasia Mexa <anastasia.mexa@yahoo.gr>
2024-02-25 17:01:10 +0200

    Send post requests using action in router.

--- code/21 Routing/12-adv-starting-project/frontend/src/App.js
+++ code/21 Routing/12-adv-starting-project/frontend/src/App.js
@@ -26,7 +26,7 @@ import EventsPage, { loader as eventsLoader } from "./pages/EventsPage";
 import EventDetailPage, {
   loader as eventDetailLoader,
 } from "./pages/EventDetailPage";
-import NewEventPage from "./pages/NewEventPage";
+import NewEventPage, {action as newEventAction } from "./pages/NewEventPage";
 import EditEventPage from "./pages/EditEventPage";
 import RootLayout from "./pages/Root";
 import EventsRootLayout from "./pages/EventsRoot";
@@ -57,7 +57,7 @@ const router = createBrowserRouter([
               { path: "edit", element: <EditEventPage /> },
             ],
           },
-          { path: "new", element: <NewEventPage /> },
+          { path: "new", element: <NewEventPage />, action: newEventAction },
         ],
       },
     ],
