02a4a0a6543907efb7b2ebaa7002edb2def22e84
Anastasia Mexa <anastasia.mexa@yahoo.gr>
2024-02-28 23:36:19 +0200

    Add checkAuthLoader to protected routes

--- code/22 Authentication/01-starting-project/frontend/src/utils/auth.js
+++ code/22 Authentication/01-starting-project/frontend/src/utils/auth.js
@@ -1,7 +1,17 @@
+import { redirect } from 'react-router-dom';
+
 export function getAuthToken() {
   return localStorage.getItem('token');
 }
 
 export function tokenLoader() {
   return getAuthToken();
+}
+
+export function checkAuthLoader() {
+  if (!getAuthToken()) {
+    return redirect('/auth?mode=login');
+  }
+
+  return null;
 }
