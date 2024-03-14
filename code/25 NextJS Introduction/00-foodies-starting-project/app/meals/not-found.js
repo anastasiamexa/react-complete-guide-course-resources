574da7c2baca1a74d81e3fabd20dd70ee9466c85
anastasiamexa <anastasia.mexa@yahoo.gr>
2024-03-14 16:18:34 +0200

    Refactor meal details page and add getMeal function

---
+++ code/25 NextJS Introduction/00-foodies-starting-project/app/meals/not-found.js
@@ -0,0 +1,8 @@
+'use client';
+
+export default function NotFound() {
+    return <main className="not-found">
+        <h1>Meal not found!</h1>
+        <p>Unfortunately, we could not find the meal you are looking for.</p>
+    </main>;
+}
