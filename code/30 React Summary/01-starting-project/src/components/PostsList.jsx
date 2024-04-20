d6595d149bcda319af741a8787016b5f43e9125f
Anastasia Mexa <anastasia.mexa@yahoo.gr>
2024-04-20 19:34:01 +0300

    Add loading functionality

--- code/30 React Summary/01-starting-project/src/components/PostsList.jsx
+++ code/30 React Summary/01-starting-project/src/components/PostsList.jsx
@@ -6,12 +6,15 @@ import { useEffect, useState } from "react";
 
 export default function PostsList({ isPosting, onStopPosting }) {
   const [posts, setPosts] = useState([]);
+  const [isFetching, setIsFetching] = useState(false);
 
   useEffect(() => {
     async function fetchPosts() {
-      const response = await fetch('http://localhost:8080/posts')
+      setIsFetching(true);
+      const response = await fetch("http://localhost:8080/posts");
       const resData = await response.json();
       setPosts(resData.posts);
+      setIsFetching(false);
     }
 
     fetchPosts();
@@ -23,7 +26,7 @@ export default function PostsList({ isPosting, onStopPosting }) {
       body: JSON.stringify(postData),
       headers: {
         "Content-Type": "application/json",
-      }
+      },
     });
     setPosts((prevPosts) => {
       return [postData, ...prevPosts];
@@ -37,17 +40,20 @@ export default function PostsList({ isPosting, onStopPosting }) {
           <NewPost onCancel={onStopPosting} onAddPost={addPostHandler} />
         </Modal>
       )}
-      <ul className={classes.posts}>
-        {posts.map((post) => (
-          <Post name={post.author} text={post.body} key={Math.random()} />
-        ))}
-      </ul>
-      {posts.length === 0 && (
+      {!isFetching && posts.length > 0 && (
+        <ul className={classes.posts}>
+          {posts.map((post) => (
+            <Post name={post.author} text={post.body} key={Math.random()} />
+          ))}
+        </ul>
+      )}
+      {!isFetching && posts.length === 0 && (
         <div style={{ textAlign: "center", color: "white" }}>
           <h2>There are no posts yet.</h2>
           <p>Start adding some!</p>
         </div>
       )}
+      {isFetching && <div style={{ textAlign: "center", color: "white" }}><p>Loading...</p></div>}
     </>
   );
 }
