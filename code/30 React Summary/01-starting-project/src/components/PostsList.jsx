447b6ee1c90dd85e781ebdeb0e635e78cc88e0b7
Anastasia Mexa <anastasia.mexa@yahoo.gr>
2024-04-20 18:41:37 +0300

    Refactor NewPost component and add onCancel functionality

--- code/30 React Summary/01-starting-project/src/components/PostsList.jsx
+++ code/30 React Summary/01-starting-project/src/components/PostsList.jsx
@@ -24,6 +24,7 @@ export default function PostsList({ isPosting, onStopPosting }) {
           <NewPost
             onBodyChange={bodyChangeHandler}
             onAuthorChange={authorChangeHandler}
+            onCancel={onStopPosting}
           />
         </Modal>
       )}
